be2d3e405e6ce2dae42e89be6c074950979878e4
ImDyslectik <robin.alkema@student.nhlstenden.com>
2024-05-21 14:56:50 +0200

    Forgot to ammend update path file

---
+++ Backend/Routes/UpdateCards.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const Project = require('../../DataSchematics/ProjectSchematic');
+
+
+router.post('/update-project', async (req, res) => {
+    try {
+        const { id, stage } = req.body;
+        console.log('Received body', req.body);
+
+        const project = await Project.findById(id);
+        if (!project) {
+            res.status(404).send({ message: 'No project with given ID found' });
+            return;
+        }
+        project.stage = stage;
+        const savedProject = await project.save();
+        res.status(200).send(savedProject);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send({ message: `Error updating project: ${err.message}` });
+    }
+});
+
+module.exports = router;
