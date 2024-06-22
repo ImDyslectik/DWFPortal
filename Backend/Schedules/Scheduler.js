887f23a6350f86aebe2e74b5f6de1d5886641539
ImDyslectik <robin.alkema@student.nhlstenden.com>
2024-06-22 12:51:27 +0200

    Replaced RSA with Hashing for save encryption + fixed export bug

--- Backend/Schedules/Scheduler.js
+++ Backend/Schedules/Scheduler.js
@@ -4,18 +4,7 @@ const sendEmailsToUsers = require('./EmailService');
 const getInactiveProjects = require('./FetchInactiveProjects');
 
 // Voor syntax, zie cronjob documentatie op: https://crontab.guru/
-// Refresh functie aanroepen elke minuut
-// const refreshJob = schedule.scheduleJob('*/5 * * * * *', function() {
-//     updateDatabase();
-// });
-
 // Email sturen elke vrijdagochtend
-const emailJob = schedule.scheduleJob('*/5 * * * * *', function() {
+const emailJob = schedule.scheduleJob('0 0 * * 5', function() {
     sendEmailsToUsers();
 });
-
-// TODO access token verversen als het nodig is
-// API functie aanroepen om access token te verversen elke nacht
-// const tokenJob = schedule.scheduleJob('0 0 * * *', function() {
-//     refreshAccesToken();
-// });
